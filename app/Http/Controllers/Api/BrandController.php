41f30f40ffacb199aba1b02b056491135802dc5c
suraj <suraj.impactmindz@gmail.com>
2025-05-06 17:08:10 +0530

    wishlistbrand

--- app/Http/Controllers/Api/BrandController.php
+++ app/Http/Controllers/Api/BrandController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Mbrand;
+use App\Models\Wishlist;
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
@@ -13,22 +14,25 @@ class BrandController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-    // Without product get brands
-        // $mbrands = Mbrand::whereNotNull('mbrand_image')
-        // ->whereIn('mbrand_id', function($query) {
-        //     $query->select('mbrand_id')->from('mproducts');
-        // })
-        // ->get();
+        $data = $request->validate([
+                'user_id' => ['required','integer','exists:users,id'],
+            ]);
+        
+        $items = Wishlist::where('user_id', $data['user_id'])
+            ->get();
 
-    // with product and variants details get brands
-        // $mbrands = Mbrand::with('mproducts.mvariants')
-        // ->whereNotNull('mbrand_image')
-        // ->whereIn('mbrand_id', function($query) {
-        //     $query->select('mbrand_id')->from('mproducts');
-        // })
-        // ->get();
+            $brands = $items
+            ->pluck('product.brand')
+            ->filter()                  
+            ->unique('mbrand_id')       
+            ->values()                   
+            ->map(fn($brand) => [       
+                'mbrand_id'   => $brand->mbrand_id,
+                'mbrand_name' => $brand->mbrand_name,
+                'mbrand_image' => $brand->mbrand_image,
+            ]);
 
         $mbrands = Mbrand::get();
 
@@ -36,7 +40,8 @@ class BrandController extends Controller
             'status' => true,
             'message'     => 'Fetch all Brands Successfully',
             'cdnURL'     => config('cdn.url'),
-            'mbrands' => $mbrands
+            'mbrands' => $mbrands,
+            'wishlistbrand' => $brands,
         ]);
     }
 
